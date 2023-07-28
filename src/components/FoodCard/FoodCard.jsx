040c13ab1f3581460555d40a6223a7d9d2821145
frankYanez <frankyanez9413@gmail.com>
2023-07-28 16:31:38 -0300

    chequeandi ramas

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,16 +1,16 @@
 
 
 const FoodCard = (props) => {
-    const { title, img , description } = props;
-    console.log(props);
+  const { title, img, description } = props;
+  console.log(props);
   return (
     <div className="bg-slate-100 border-2 border-white relative rounded-tl-2xl rounded-br-2xl w-64 h-72 rounded-bl-sm">
-        <img className="absolute -top-28 inset-x-5 " src={img} alt="" />
-        <div className=" absolute inset-y-28">
+      <img className="absolute -top-28 inset-x-5 " src={img} alt="" />
+      <div className=" absolute inset-y-28">
         <h3 className="font-bold text-center">{title}</h3>
         <p className="text-center">{description}</p>
-        </div>
-        
+      </div>
+
     </div>
   )
 }
